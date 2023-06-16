8b1127e37fcb82fff3347fe0b8b12ad96160447f
huangtianyou <1156388927@qq.com>
2023-06-17 07:41:56 +0800

    更新今日每日一题

---
+++ leetcode/每日一题/2023/06/17/solution.java
@@ -0,0 +1,12 @@
+//2481. 分割圆的最少切割次数
+class Solution {
+    public int numberOfCuts(int n) {
+        if (n == 1) {
+            return 0;
+        }
+        if (n % 2 == 0) {
+            return n / 2;
+        }
+        return n;
+    }
+}
