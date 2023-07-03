28c4d1cb12e4964cbe850287a6264a692f1d6204
huangtianyou <1156388927@qq.com>
2023-07-03 10:37:14 +0800

    更新今日每日一题

---
+++ leetcode/每日一题/2023/07/03/solution.java
@@ -0,0 +1,28 @@
+//445. 两数相加 II
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) { 
+        Stack<Integer> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+        while (l1 != null) {
+            stack1.push(l1.val);
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            stack2.push(l2.val);
+            l2 = l2.next;
+        }
+        
+        int carry = 0;
+        ListNode head = null;
+        while (!stack1.isEmpty() || !stack2.isEmpty() || carry > 0) {
+            int sum = carry;
+            sum += stack1.isEmpty()? 0: stack1.pop();
+            sum += stack2.isEmpty()? 0: stack2.pop();
+            ListNode node = new ListNode(sum % 10);
+            node.next = head;
+            head = node;
+            carry = sum / 10;
+        }
+        return head;
+    }
+}
