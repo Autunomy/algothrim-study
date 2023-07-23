5a13cff7d1f7f07f84b4cb95c320b0a3aaef864d
huangtianyou <1156388927@qq.com>
2023-07-23 10:03:19 +0800

    更新今日每日一题

---
+++ leetcode/每日一题/2023/07/23/solution.java
@@ -0,0 +1,23 @@
+//42. 接雨水
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+        leftMax[0] = height[0];
+        rightMax[n-1] = height[n-1];
+        for(int i=1;i<n;++i){
+            leftMax[i] = Math.max(leftMax[i-1],height[i]); 
+        }
+        for(int i=n-2;i>=0;--i){
+            rightMax[i] = Math.max(rightMax[i+1],height[i]); 
+        }
+
+        int res = 0;
+        for(int i=1;i<n-1;++i){
+            res += Math.min(leftMax[i],rightMax[i]) - height[i];
+        }
+
+        return res;
+    }
+}
