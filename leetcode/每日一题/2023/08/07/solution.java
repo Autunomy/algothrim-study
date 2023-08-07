1976c15154acd788a14bdb7d86af0c55ee4d122a
黄天佑 <i_huangtianyou@cvte.com>
2023-08-07 08:49:32 +0800

    更新今日每日一题

---
+++ leetcode/每日一题/2023/08/07/solution.java
@@ -0,0 +1,13 @@
+//344. 反转字符串
+class Solution {
+    public void reverseString(char[] s) {
+        int i = 0,j = s.length-1;
+        while(i < j){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
