256e1af4515e293caaf4063140733951090a76be
黄天佑 <i_huangtianyou@cvte.com>
2023-08-08 09:14:08 +0800

    更新今日每日一题

---
+++ leetcode/每日一题/2023/08/08/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxAbsoluteSum(int[] nums) {
+        int ans = 0, fMax = 0, fMin = 0;
+        for (int x : nums) {
+            fMax = Math.max(fMax, 0) + x;
+            fMin = Math.min(fMin, 0) + x;
+            ans = Math.max(ans, Math.max(fMax, -fMin));
+        }
+        return ans;
+    }
+}
