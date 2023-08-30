fb82de683d9c3bbb1a4370af514145485d3cdede
huangtianyou <1156388927@qq.com>
2023-08-30 09:57:55 +0800

    更新今日每日一题

---
+++ leetcode/每日一题/2023/08/30/solution.java
@@ -0,0 +1,36 @@
+//1654.到家的最少跳跃次数
+class Solution {
+    public int minimumJumps(int[] forbidden, int a, int b, int x) {
+        Queue<int[]> queue = new ArrayDeque<int[]>();
+        Set<Integer> visited = new HashSet<Integer>();
+        queue.offer(new int[]{0, 1, 0});
+        visited.add(0);
+        int lower = 0, upper = Math.max(Arrays.stream(forbidden).max().getAsInt() + a, x) + b;
+        Set<Integer> forbiddenSet = new HashSet<Integer>();
+        for (int position : forbidden) {
+            forbiddenSet.add(position);
+        }
+        while (!queue.isEmpty()) {
+            int[] arr = queue.poll();
+            int position = arr[0], direction = arr[1], step = arr[2];
+            if (position == x) {
+                return step;
+            }
+            int nextPosition = position + a;
+            int nextDirection = 1;
+            if (lower <= nextPosition && nextPosition <= upper && !visited.contains(nextPosition * nextDirection) && !forbiddenSet.contains(nextPosition)) {
+                visited.add(nextPosition * nextDirection);
+                queue.offer(new int[]{nextPosition, nextDirection, step + 1});
+            }
+            if (direction == 1) {
+                nextPosition = position - b;
+                nextDirection = -1;
+                if (lower <= nextPosition && nextPosition <= upper && !visited.contains(nextPosition * nextDirection) && !forbiddenSet.contains(nextPosition)) {
+                    visited.add(nextPosition * nextDirection);
+                    queue.offer(new int[]{nextPosition, nextDirection, step + 1});
+                }
+            }
+        }
+        return -1;
+    }
+}
