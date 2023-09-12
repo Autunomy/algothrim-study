828ba538a29407062c87e6d573b90e8f659520ae
huangtianyou <1156388927@qq.com>
2023-09-12 09:51:52 +0800

    更新今日每日一题

---
+++ leetcode/每日一题/2023/09/12/solution.java
@@ -0,0 +1,39 @@
+//1462. 课程表 IV
+class Solution {
+    public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
+        List<Integer>[] g = new List[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            g[i] = new ArrayList<Integer>();
+        }
+        int[] indgree = new int[numCourses];
+        boolean[][] isPre = new boolean[numCourses][numCourses];
+        for (int[] p : prerequisites) {
+            ++indgree[p[1]];
+            g[p[0]].add(p[1]);
+        }
+        Queue<Integer> queue = new ArrayDeque<Integer>();
+        for (int i = 0; i < numCourses; ++i) {
+            if (indgree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            for (int ne : g[cur]) {
+                isPre[cur][ne] = true;
+                for (int i = 0; i < numCourses; ++i) {
+                    isPre[i][ne] = isPre[i][ne] | isPre[i][cur];
+                }
+                --indgree[ne];
+                if (indgree[ne] == 0) {
+                    queue.offer(ne);
+                }
+            }
+        }
+        List<Boolean> res = new ArrayList<Boolean>();
+        for (int[] query : queries) {
+            res.add(isPre[query[0]][query[1]]);
+        }
+        return res;
+    }
+}
